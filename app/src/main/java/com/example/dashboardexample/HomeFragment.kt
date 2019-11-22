c13c482db8b0cdc1d5a22b0a6e5060fbd8a2129c
IN007848 <Reshma@oppo2019>
2019-11-22 18:13:32 +0530

    Initial commit

---
+++ app/src/main/java/com/example/dashboardexample/HomeFragment.kt
@@ -0,0 +1,26 @@
+package com.example.dashboardexample
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+
+class HomeFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_home, container, false)
+    }
+
+
+   // https://stackoverflow.com/questions/30180957/send-post-request-with-params-using-retrofit
+   // https://www.youtube.com/watch?v=VIucl1jcJ-I
+   // https://www.youtube.com/watch?v=Y8mGxxRe3k4
+
+    }
+
